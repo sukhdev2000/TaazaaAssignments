2c2bb493cdbe47143fa102816967a94142368f8a
sukhdev2000[ <dsukhdev2000@gmail.com>
2021-08-24 18:27:02 +0530

    trying

---
+++ Assignment-8/StringBasics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter You Name!!");
+           string str=Console.ReadLine();
+           System.Console.WriteLine("Welcome "+ str);
+
+        }
+    }
+}
