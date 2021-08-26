2a069011c62b454aa6e85eda73d5bd661d72104e
sukhdev2000[ <dsukhdev2000@gmail.com>
2021-08-26 18:05:48 +0530

    Delegates

---
+++ Assignment-8/DelegatePractice/Arithmetic/ArithmeticOperation.cs
@@ -0,0 +1,20 @@
+using System;
+using DelegatePractice;
+namespace DelegatePractice.Arithmetic
+{
+    public class ArithmeticOperation
+    {
+        public static void Addition(double number1, double number2)
+        {
+            Console.WriteLine("Addition "+(number1+number2));
+        } 
+        /* public static void Divide(double number1, double number2)
+        {
+            Console.WriteLine("Division "+(number1/number2));
+        } 
+        public static void Multiply(double number1, double number2)
+        {
+            Console.WriteLine("Multiply "+(number1*number2));
+        } */ 
+    }
+}
---
+++ Assignment-8/DelegatePractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DelegatePractice.Arithmetic;
+delegate void ArithmeticDelegate(double N1,double N2);
+namespace DelegatePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*  //first way
+            ArithmeticDelegate obj=new ArithmeticDelegate(ArithmeticOperation.Addition);
+            obj.Invoke(5,4);
+            //Anonymous method(Second Way)
+           ArithmeticDelegate obj1=delegate(double d1,double d2)
+           {
+               Console.WriteLine("Multiply "+(d1*d2));
+           };
+           ArithmeticDelegate obj2=delegate(double d1,double d2)
+           {
+               Console.WriteLine("Division "+(d1/d2));
+           }; */
+
+
+           //Lambda method (third way)
+           ArithmeticDelegate obj1=(double d1,double d2)=>
+           {
+               Console.WriteLine("Multiply "+(d1*d2));
+           };
+           ArithmeticDelegate obj2=(double d1,double d2)=>
+           {
+               Console.WriteLine("Division "+(d1/d2));
+           };
+           obj1(10,20);
+           obj2(20,5);
+        }
+    }
+}
